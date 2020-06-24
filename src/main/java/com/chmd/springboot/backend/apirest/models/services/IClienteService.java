559ea252b53a3a325683748c1f13823a6ac11c04
cmunoz780 <cristian.munoz.donoso@gmail.com>
2020-06-23 21:21:47 -0400

    inicio repo

---
+++ src/main/java/com/chmd/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,11 @@
+package com.chmd.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.chmd.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	
+	public List<Cliente> findAll();
+
+}
